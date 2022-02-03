04c52c59a01ec84d32511db0ffc2ffe9d68726ef
sonia0409 <sonia.singh0409@gmail.com>
2022-02-03 16:49:21 -0500

    update redirect paths

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,11 @@ class SessionsController < ApplicationController
     def create 
         user = User.find_by_email(params[:email])
 
-        if user && user.authenticate(param[:password])
+        if user && user.authenticate(params[:password])
             session[:user_id] = user.id
             redirect_to '/'
         else
-            render :new
+            render '/login'
         end
     end
 
